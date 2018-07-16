d41eaaebdab883bec69ef84005d29291175c00ec
halleri <halleri@users.noreply.github.com>
2018-07-16 16:42:52 -0700

    Create testDrawCard.py

---
+++ testDrawCard.py
@@ -0,0 +1,9 @@
+import unittest
+from drawCard import drawCard
+
+class TestCard(unittest.TestCase):
+    def setUp(self):
+        self.actual = range(1,53)
+
+if __name__ == '__main__':
+    unittest.main()
